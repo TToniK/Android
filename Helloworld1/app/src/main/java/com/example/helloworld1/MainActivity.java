15a39440cd6ed6e50705123519c922521936b38c
Toni Kari <TToniK@users.noreply.github.com>
2020-10-28 15:06:24 +0200

    Button hides textView and makes it visible again. Also app icon has been added.

--- Helloworld1/app/src/main/java/com/example/helloworld1/MainActivity.java
+++ Helloworld1/app/src/main/java/com/example/helloworld1/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.helloworld1;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt;
+    Button bestButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.textView);
+        bestButton = findViewById(R.id.bestButton);
+        bestButton.setOnClickListener(new View.OnClickListener(){
+            boolean visible;
+            @Override
+            public void onClick(View view) {
+                visible = !visible;
+                txt.setVisibility(visible ? View.VISIBLE: View.GONE);
+                
+            }
+
+
+        });
+
     }
+
+
+
+
+
+
 }
