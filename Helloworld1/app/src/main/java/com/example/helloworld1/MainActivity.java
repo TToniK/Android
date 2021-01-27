eae8ef851de6a19763fa5776aee79b11d9c7394e
Toni Kari <TToniK@users.noreply.github.com>
2021-01-27 11:58:31 +0200

    yrityksen tynkää

--- Helloworld1/app/src/main/java/com/example/helloworld1/MainActivity.java
+++ Helloworld1/app/src/main/java/com/example/helloworld1/MainActivity.java
@@ -19,12 +19,18 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 
+import com.example.helloworld1.ui.dashboard.DashboardFragment;
+import com.example.helloworld1.ui.home.HomeFragment;
+import com.example.helloworld1.ui.notifications.NotificationsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txt;
     Button bestButton;
     Button gameButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +39,22 @@ public class MainActivity extends AppCompatActivity {
         txt = findViewById(R.id.textView);
         bestButton = findViewById(R.id.bestButton);
         gameButton = findViewById(R.id.gameButton);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
         gameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openGameActivity();
             }
-            public void openGameActivity(){
-                Intent intent = new Intent (MainActivity.this, GameActivity.class);
+
+            public void openGameActivity() {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(intent);
             }
 
         });
+
+
         bestButton.setOnClickListener(new View.OnClickListener() {
             boolean visible;
 
@@ -56,7 +67,33 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-    }};
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.navigation_dashboard:
+                            selectedFragment = new DashboardFragment();
+                            break;
+                        case R.id.navigation_notifications:
+                            selectedFragment = new NotificationsFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
+
+}
 
 
 
